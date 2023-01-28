b0fe8b4456ea29ebd91de69b996b7545962fc7f1
jsa-eriques <jsa.eric@hotmail.com>
2023-01-27 23:53:09 -0300

    content reforce - ALL tasks day02 again with another metods +/-

--- mf/mf-3/day2/exercicio-lidando-com-arrays.js
+++ mf/mf-3/day2/exercicio-lidando-com-arrays.js
@@ -68,3 +68,129 @@
 //     numbers.push(i);
 // }
 // console.log(numbers)
+
+
+
+
+
+/* making exercices again, again, again */
+
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for ( i = 0; i < numbers.length; i += 1 ) {
+//     console.log(numbers[i]);
+// }
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma= 0;
+
+// for ( i = 0; i < numbers.length; i += 1 ) {
+    // soma += numbers[i]; /* Somatoria = 278 */   
+// }
+//  console.log(soma);
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma= 0;
+
+// for ( i = 0; i < numbers.length; i += 1 ) {
+//     soma += numbers[i];
+ 
+// }
+//     console.log(soma); // Media 278 / 10 = 27.8
+//     console.log(soma / i); // Media 278 / 10 = 27.8
+//
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem:
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma= 0;
+
+// for ( i = 0; i < numbers.length; i += 1 ) {
+//     soma += numbers[i];
+// }
+//     console.log(soma); // Media 278 / 10 = 27.8
+//     console.log(soma / i); // Media 278 / 10 = 27.8 
+//  if (soma > 20){
+//     console.log(`Valor maior que 20!`)
+// } else {
+//     console.log(`Valor menor ou igual a 20!`)
+// } 
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let higherNumber = numbers[0];
+
+// for ( i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > higherNumber) {
+//         higherNumber = numbers[i]; 
+//     }
+   
+// }
+// console.log(higherNumber)
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. 
+// Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let isPair;
+// let isOdd;
+// let OddsCount = 0;
+// let PairsCount = 0;
+
+// for ( i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 !== 0) {
+//         OddsCount +=1;
+//         isPair = `${numbers[i]} é ímpar!`;
+//         // console.log(numbers[i]);
+//     }else {
+//         PairsCount +=1;
+//         isPair = `${numbers[i]} é par!`;
+//         // console.log(numbers[i]);
+//     }
+//     console.log(isPair )
+// }
+//     console.log(`Temos um Quantitativo de Números Ímpares = ${OddsCount}`)
+//     console.log(`Temos um Quantitativo de Números Pares = ${PairsCount}`)
+//
+
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let minorValue = numbers[0];
+
+// for ( i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] < minorValue){
+//         minorValue = numbers[i]
+//     }
+// }
+// console.log(minorValue);
+
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+
+// let soma;
+
+// for ( i = 1; i <= 25; i += 1) {
+//         array.push(i);
+//     }
+// console.log(array)
+
+// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+//
+
+// let array = [];
+// for ( i = 1; i <= 25; i += 1) {
+//         array.push(i);
+//         array / 2;
+//     }
+// console.log(array)
+//
+//
